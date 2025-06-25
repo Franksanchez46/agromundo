cefbbc5f6eca3dcddf0c406b47488b90c3a62a76
Franksanchez46 <fsanchezaroca@gmail.com>
2025-06-25 14:24:11 -0500

    cambio servidor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        view::composer('partials.categorias', function ($view){
+        View::composer('partials.categorias', function ($view){
             $view->with('categorias', Categoria::all());
         });
 
