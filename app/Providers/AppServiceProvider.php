696668bd508294e45bc8f839fac942f5f4fd57b1
Franksanchez46 <fsanchezaroca@gmail.com>
2025-06-25 14:28:00 -0500

    serve 1,2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\view;
+use Illuminate\Support\Facades\View;
 use App\Models\Categoria;
 use App\Models\Oferta;
 
